d702bd481d0fbb4e1b17d5fd66f381aba398ae2d
zschmidt258 <zschmidt258@cedarville.edu>
2024-03-22 15:11:41 -0400

    fOrMaT

--- src/parser/Expression.java
+++ src/parser/Expression.java
@@ -2,7 +2,19 @@ package src.parser;
 
 public abstract class Expression implements Node{
 
-    public static enum Operator { LTE, LT, GT, GTE, EQ, NEQ, PLUS, ADD, SUB, MUL, DIV };
+    public static enum Operator { 
+        LTE, 
+        LT, 
+        GT, 
+        GTE, 
+        EQ, 
+        NEQ, 
+        PLUS, 
+        ADD, 
+        SUB, 
+        MUL, 
+        DIV 
+    };
 
     public abstract String printNode(int indent);
 }
